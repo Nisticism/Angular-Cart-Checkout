a97cf97b292df6ee60e258116c33a16013d571fc
nisticism <nistacular@gmail.com>
2020-12-30 16:31:57 -0800

    testing gitignore

--- src/app/components/confirmation/confirmation.component.ts
+++ src/app/components/confirmation/confirmation.component.ts
@@ -18,7 +18,7 @@ export class ConfirmationComponent implements OnInit {
 
   confirmation = (digits: number) => function() {
   //  Randomly generate a number with some amount of digits
-    var num: number = Math.floor((Math.random() * 9 * (10**(digits - 1)) + 10**(digits - 1))); 
+    let num: number = Math.floor((Math.random() * 9 * (10**(digits - 1)) + 10**(digits - 1))); 
     return num
   }
 
